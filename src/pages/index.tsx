3b51ab123c748ff982394fef2611de6db108ecdd
jdawsonimgix <jdawson@imgix.com>
2022-04-06 14:19:25 -0700

    updated react component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -179,22 +179,22 @@ const Index = () => {
         {ExampleFiles.map((file) => (
           <li key={file.source} className="relative">
             <div className="group  block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+              <Imgix
+                src={file.source}
+                imgixParams={{ auto: 'format,compress' }}
+                sizes="(min-width: 36em) 33.3vw, 100vw"
+              />
               {/* <img
                 src={file.source}
                 alt=""
                 className="pointer-events-none object-cover group-hover:opacity-75"
               /> */}
-              <button
+              {/* <button
                 type="button"
                 className="absolute inset-0 focus:outline-none"
               >
                 <span className="sr-only">View details for {file.title}</span>
-              </button>
-              <Imgix
-                src={file.source}
-                imgixParams={{ auto: 'format,compress' }}
-                sizes="20vw"
-              />
+              </button> */}
             </div>
             <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
               {file.title}
