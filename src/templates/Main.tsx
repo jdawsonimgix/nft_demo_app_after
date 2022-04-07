a4bc38854d2ed166f2a98e3b36e1ad8e704365e5
jdawsonimgix <jdawson@imgix.com>
2022-04-07 15:11:26 -0700

    updated page to show before and after tabs on same app

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -24,27 +24,27 @@ const Main = (props: IMainProps) => (
         <div>
           <ul className="flex flex-wrap text-xl">
             <li className="mr-6">
+              <Link href="/before/">
+                <a className="border-none text-gray-700 hover:text-gray-900">
+                  Before
+                </a>
+              </Link>
+            </li>
+            <li className="mr-6">
               <Link href="/">
                 <a className="border-none text-gray-700 hover:text-gray-900">
-                  Home
+                  After
                 </a>
               </Link>
             </li>
-            <li className="mr-6">
-              <Link href="/about/">
-                <a className="border-none text-gray-700 hover:text-gray-900">
-                  About
-                </a>
-              </Link>
-            </li>
-            <li className="mr-6">
+            {/* <li className="mr-6">
               <a
                 className="border-none text-gray-700 hover:text-gray-900"
                 href="https://github.com/ixartz/Next-js-Boilerplate"
               >
                 GitHub
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
       </div>
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Imgix NFT dmeo app',
-  title: 'Imgix NFT demo app - After optimization',
-  description: `A demo app built in Nextjs to show NFT's after they are optimized.`,
+  title: 'Imgix NFT demo app - before and after optimization',
+  description: `A demo app built in Nextjs to show NFT's being optimized.`,
   locale: 'en',
 };
